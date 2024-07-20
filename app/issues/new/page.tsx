b6751b36da31412a572e0764553e93e6c1407f11
Nelis <neliseerdekens1@hotmail.com>
2024-07-21 00:07:45 +0200

    new issue page done

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { TextField, TextArea, Button } from "@radix-ui/themes";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="title"></TextField.Root>
+      <TextArea placeholder="description" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
