a9deb3ca458308bceee3e8ec865d9844544d0f4f
Nelis <neliseerdekens1@hotmail.com>
2024-07-23 21:51:18 +0200

    everything up until form submit, but issues with api connection

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,14 +4,41 @@ import React from "react";
 import { TextField, Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  console.log(register("title"));
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="title"></TextField.Root>
-      <SimpleMDE placeholder="description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root
+        placeholder="title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="description" {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
